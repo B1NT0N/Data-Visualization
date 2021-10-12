8fa11b1cf0d622c5ed70881f8eb3bd272f340fe8
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-10-12 18:09:02 -0100

    Config Setup Update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]
+base='light'\n\
+primaryColor='#1cb4c4'\n\
 [server]\n\
+maxUploadSize = 250\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
